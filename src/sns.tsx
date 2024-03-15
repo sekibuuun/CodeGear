92e9fae8fea014d699c40f89fe5b2bad71f59409
Aoyama Kazuki <sekibun3109@gmail.com>
2024-03-15 22:58:52 +0900

    feat: プロパティの追加

--- src/sns.tsx
+++ src/sns.tsx
@@ -1,7 +1,7 @@
 import GitHub from "./assets/GitHub.svg";
 import Instagram from "./assets/Instagram.svg";
 import Qiita from "./assets/Qiita.png";
-import X from "./assets/X.svg";
+import X from "./assets/X.png";
 import Zenn from "./assets/Zenn.svg";
 import { Social } from "./types";
 
@@ -10,31 +10,37 @@ const socials: Social[] = [
     service: "X",
     url: "https://twitter.com/",
     logo: X,
+    color: "#0f1419",
   },
   {
     service: "Instagram",
     url: "https://www.instagram.com/",
     logo: Instagram,
+    color: "#E1306C",
   },
   {
     service: "GitHub",
     url: "https://github.com/",
     logo: GitHub,
+    color: "#171515",
   },
   {
     service: "Zenn",
     url: "https://zenn.dev/",
     logo: Zenn,
+    color: "#3EA8FF",
   },
   {
     service: "Qiita",
     url: "https://qiita.com/",
     logo: Qiita,
+    color: "#67CB1B",
   },
   {
     service: "AtCoder",
     url: "https://atcoder.jp/users/",
     logo: null,
+    color: "#494649",
   },
 ];
 
