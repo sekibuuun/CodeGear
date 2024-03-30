f2ec38d473048780bf4eba7fa95bb232b27651dd
Aoyama Kazuki <sekibun3109@gmail.com>
2024-03-30 09:40:30 +0900

    rem: ヘッダーを一時変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { Icon } from "./icon";
-import { Modal } from "./modal";
+// import { Icon } from "./icon";
+// import { Modal } from "./modal";
 
 const Header: React.FC = () => {
   return (
-    <div className="px-10 flex justify-between items-center h-14 bg-gray-800 text-white">
-      <Icon src="" fallback="CN" />
+    <div className="px-10 flex justify-center items-center h-14 bg-gray-800 text-white">
+      {/* <Icon src="" fallback="CN" /> */}
       <h1 className="text-2xl">CodeGear</h1>
-      <Modal />
+      {/* <Modal /> */}
     </div>
   );
 };
