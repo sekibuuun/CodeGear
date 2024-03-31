28c4d8b61d4f0ef10019ecb167820b24cc7864f3
Aoyama Kazuki <sekibun3109@gmail.com>
2024-03-31 16:31:34 +0900

    fix: iconがgithubのiconにした

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import { Icon } from "./icon";
 import { Modal } from "./modal";
 
+import { useUser } from "@/hooks/useUser";
+
 const Header: React.FC = () => {
+  const { session } = useUser();
   return (
     <div className="px-10 flex justify-between items-center h-14 bg-gray-800 text-white">
-      <Icon src="" fallback="CN" />
+      <Icon src={session?.user.user_metadata.avatar_url} fallback="CN" />
       <h1 className="text-2xl">CodeGear</h1>
       <Modal />
     </div>
