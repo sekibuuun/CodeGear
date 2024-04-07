7fdf8170d35f627993ddd971e4682b647532d127
Aoyama Kazuki <sekibun3109@gmail.com>
2024-04-07 14:54:31 +0900

    fix: mypageのUI改修

--- src/components/mypage.tsx
+++ src/components/mypage.tsx
@@ -9,14 +9,16 @@ const Mypage: React.FC<{
   const QRData = useQRData(id);
 
   return (
-    <div>
-      <div>{name}</div>
+    <div className="mypage px-10">
+      <div className="text-2xl font-bold my-3">{name}&apos;s QRCode</div>
       <Suspense fallback={<div>Loading...</div>}>
-        <div>
+        <div className="flex flex-col md:flex-row md:flex-wrap">
           {QRData.map((qr, index) => (
-            <div key={index}>
-              <p>{qr.qr_code_type}</p>
-              <img src={qr.qr_code_image} alt="QR Code" />
+            <div key={index} className="w-full md:w-1/2 lg:w-1/3 p-2">
+              <p className="flex justify-center text-xl font-bold">
+                {qr.qr_code_type}
+              </p>
+              <img src={qr.qr_code_image} alt="QR Code" className="mx-auto" />
             </div>
           ))}
         </div>
